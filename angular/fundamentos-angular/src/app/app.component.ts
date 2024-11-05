7bee61ad545145eb5ca80c32d381e0d19a263156
Gabriel Terto <gabrielterto1002@gmail.com>
2024-11-05 15:20:02 -0300

    Adicionado two-way-data-binding

--- angular/fundamentos-angular/src/app/app.component.ts
+++ angular/fundamentos-angular/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { PrimeiroComponenteComponent } from "./primeiro-componente/primeiro-componente.component";
 import { SegundoComponenteComponent } from "./segundo-componente/segundo-componente.component";
 import { PipeCustomizadoComponent } from "./pipe-customizado/pipe-customizado.component";
+import { TwoWayDataBindingComponent } from "./two-way-data-binding/two-way-data-binding.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PrimeiroComponenteComponent, SegundoComponenteComponent, PipeCustomizadoComponent],
+  imports: [RouterOutlet, PrimeiroComponenteComponent, SegundoComponenteComponent, PipeCustomizadoComponent, TwoWayDataBindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
